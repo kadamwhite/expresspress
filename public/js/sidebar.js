df2a365cf921ec1d0447575407482b38619ea42e
K.Adam White <kadamwhite@gmail.com>
2014-10-25 10:57:23 -0700

    Add sidebar tag toggler button, using jquery

---
+++ public/js/sidebar.js
@@ -0,0 +1,18 @@
+(function( $ ) {
+  var $moreTags = $( '#more-tags' );
+  var $toggleTagsButton = $( '#toggle-all-tags' );
+
+  $toggleTagsButton.on( 'click', function( evt ) {
+    $moreTags.slideToggle({
+      done: function() {
+        // Once the animation is done, switch to class-based hiding
+        // so that the lists line up better
+        $moreTags
+          .toggleClass( 'collapsed' )
+          .removeAttr( 'style' );
+      }
+    });
+    // Flip label in the button
+    $toggleTagsButton.find( 'span' ).toggleClass( 'hidden' );
+  });
+})( jQuery );
